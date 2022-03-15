26d30ea7046276cc0260e37e70a888514e6180b2
Gerardo Merida <cgemerida@gmail.com>
2022-03-15 12:08:47 -0600

    test(vehicle model): add new properties

--- src/models/vehicle.spec.ts
+++ src/models/vehicle.spec.ts
@@ -1,16 +1,20 @@
 import Vehicle from "./vehicle";
 
 describe("Vehicle Model", () => {
-  const testVehicle = { name: "Nissan", model: "DXT", year: 2020 };
+  const testVehicle = {
+    name: "Nissan",
+    model: "DXT",
+    year: 2020,
+    price: 25.2,
+    inventory: true,
+  };
   it("should create a new vehicle with passed params", () => {
-    const vehicle = new Vehicle(
-      testVehicle.name,
-      testVehicle.model,
-      testVehicle.year
-    );
+    const vehicle = new Vehicle(testVehicle);
 
     expect(vehicle.name).toBe(testVehicle.name);
     expect(vehicle.model).toBe(testVehicle.model);
     expect(vehicle.year).toBe(testVehicle.year);
+    expect(vehicle.price).toBe(testVehicle.price);
+    expect(vehicle.inventory).toBe(testVehicle.inventory);
   });
 });
