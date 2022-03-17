dce7e19bd39281400ce0a68542eb5aa364f5ddd9
Gerardo Merida <cgemerida@gmail.com>
2022-03-17 00:11:37 -0600

    feat(email validator): add email validator Interface and adapter

---
+++ src/adapters/email-validator.ts
@@ -0,0 +1,15 @@
+import Joi from 'joi';
+import IEmailValidator from '../protocols/email-validator';
+
+const emailSchema = Joi.string().email().lowercase().required();
+
+export default class EmailValidatorAdapter implements IEmailValidator {
+  isValid(email: string): boolean {
+    try {
+      Joi.assert(email, emailSchema);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+}
